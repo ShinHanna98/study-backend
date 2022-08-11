969da33b7cea941e077301fa0e4d612373c90871
ShinHanna98 <tls1593575@gachon.ac.kr>
2022-08-11 17:44:24 +0900

    study: nodejs template

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
-
+app.set("view engine", "ejs")
+app.use("/static", express.static("static"))
 const port = 8000;
-// localhost:8000/
+
+
 app.get("/", (req,res)=>{
-    res.sendFile(__dirname+"/test.html");
+    var fruits = [
+        { fruit: "apple", color: "red" },
+        { fruit: "banana", color: "yellow" },
+        { fruit: "grape", color: "purple" }
+    ]
+    res.render("test", {fru: fruits});
 })
 
+// localhost:8000/test
+app.get("/test", (req,res)=>{
+    res.render("test1");
+})
 app.listen(port, ()=>{
     console.log("server open: ", port);
 })
